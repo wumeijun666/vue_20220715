3db9d9ec0e6cd9ef376bcbe63c8ec8475dc0b75c
seven <1073765851@qq.com>
2022-07-17 00:32:22 +0800

    02_src_ref属性

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,7 @@ module.exports = {
     pages: {
       index: {
         // page 的入口
-        entry: 'src/peiqi.js',
-      
+        entry: 'src/peiqi.js'
       },
   },
   lintOnSave:false//关闭语法检查
