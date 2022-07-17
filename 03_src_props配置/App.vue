34615517dfb790ba38c78012e4c22254b8b0fc56
seven <1073765851@qq.com>
2022-07-17 22:27:25 +0800

    03_src_props配置

---
+++ 03_src_props配置/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<Student name="李四" sex="aaa" :age="23"></Student>
+	</div>
+</template>
+
+<script>
+	//引入School组件
+	import Student from './components/Student.vue'
+
+	export default {
+		name:'App',
+		components:{Student},
+	}
+</script>
