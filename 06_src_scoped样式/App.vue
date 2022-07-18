44bc27c5081ac2886ef4f63a06a67b32446fe1d1
seven <1073765851@qq.com>
2022-07-18 22:28:33 +0800

    06_src_scoped样式

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		<h2 class="title"><span class="qwe">学习</span></h2>
+		<Student ></Student>
+		<hr />
+		<School></School>
+	</div>
+</template>
+
+<script>
+
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+
+export default {
+	name: 'App',
+	components: { Student, School },
+}
+</script>
+<style lang="less" scoped>
+	.title{
+		color:red;
+		.qwe{
+			font-size: 40px;
+		}
+	}
+</style>
---
+++ 06_src_scoped样式/components/School.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="demo">
+		<h2>学校名称：{{ name}}</h2>
+		<h2>学校地址：{{ address }}</h2>
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'School',
+	data() {
+		return {
+			name: '尚硅谷assffs',
+			address: '北京',
+		}
+	},
+	methods: {
+	}
+}
+</script>
+<style scoped>
+	.demo{
+		background-color: skyblue;
+	}
+</style>
